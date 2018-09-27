d3b24cf43bb2407269c43799e132b2d8a644b8a2
skydraft <wertd@mail.ru>
2018-09-27 19:39:50 +0700

    Работа со структурами

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    /*
+    Создайте структуру MathStruct, вложенную в класс MyClass, и напишите для нее метод Square, который будет принимать значение число типа int и возвращать его квадрат.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.MathStruct instance = new MyClass.MathStruct();
+            int square = instance.Square(7);
+            Console.WriteLine(square);
+
+            Console.ReadKey();
+        }
+    }
+
+    
+    class MyClass
+    {
+        //nested(вложенная) структура MathStruct в класс MyClass
+        public struct MathStruct
+        {
+           
+            public int Square(int i)
+            {
+                return i * i;
+            }
+        }
+    }
+}
