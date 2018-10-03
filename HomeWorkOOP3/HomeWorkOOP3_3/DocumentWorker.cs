dd69e23f027a0f97e70886bad067c30bcdf3eaf2
skydraft <wertd@mail.ru>
2018-10-03 20:45:19 +0700

    выполнено четвертое задание(используя if-else)

---
+++ HomeWorkOOP3/HomeWorkOOP3_3/DocumentWorker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOOP3_3
+{
+    class DocumentWorker
+    {
+       public void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт");
+        }
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редактирование документа доступно в версии Про");
+        }
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Сохранение документа доступно в версии Про");
+        }
+
+    }
+}
---
+++ HomeWorkOOP3/HomeWorkOOP3_3/ExpertDocumentWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOOP3_3
+{
+    class ExpertDocumentWorker:ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+
+    }
+}
