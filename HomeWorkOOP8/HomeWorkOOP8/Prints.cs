4316fc8cb08263375ebf43ff356c813516e224d5
skydraft <wertd@mail.ru>
2018-10-09 21:25:28 +0700

    выполнено восьмое задание(структуры и перечисления enum)

---
+++ HomeWorkOOP8/HomeWorkOOP8/Prints.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOOP8
+{
+    static class Prints
+    {
+        //создаем перечесление
+        enum ColorEnum:int
+        {
+            Black = 0,
+            DarkBlue = 1,
+            DarkGreen = 2,
+            DarkCyan = 3,
+            DarkRed = 4,
+            DarkMagenta = 5,
+            DarkYellow = 6,
+            Gray = 7,
+            DarkGray = 8,
+            Blue = 9,
+            Green = 10,
+            Cyan = 11,
+            Red = 12,
+            Magenta = 13,
+            Yellow = 14,
+            White = 15
+        }
+      public static void Print(string stroka, int color)
+        {
+
+            ColorEnum colorEnum;
+            //color приводим к типу ColorEnum
+            colorEnum =(ColorEnum)color;
+            //передаем значение colorEnum(colorEnum приводим к типу ConsoleColor)
+            Console.ForegroundColor = (ConsoleColor)colorEnum;
+            //вывод строки
+            Console.WriteLine(stroka);
+            
+            
+        }
+    }
+}
---
+++ HomeWorkOOP8/HomeWorkOOP8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOOP8
+{
+    /*Задание
+     * Создайте статический класс с методом void Print (string stroka, int color),
+     * который выводит на экран строку заданным цветом.
+     * Используя перечисление, создайте набор цветов, доступных пользователю.
+     * Ввод строки и выбор цвета предоставьте пользователю. 
+     * 
+     */
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color;
+            string text;
+            int colorInt;
+            //запрос цвета и строки для вывода 
+            Console.WriteLine("Выберите цвет(от 0 до 15)");
+            color = Console.ReadLine();
+            Console.WriteLine("Введите текст для вывода");
+            text = Console.ReadLine();
+            //проверяем на допустимые значения
+            if (Int32.TryParse(color, out colorInt))
+            {
+                colorInt = Convert.ToInt32(color);
+                if (0 <= colorInt & colorInt <= 15)
+                {
+                    Console.WriteLine(new string('-', 30));
+                    Prints.Print(text, colorInt);
+                }
+                else { Console.WriteLine("Введен недопустимый цвет"); }
+            }
+            else { Console.WriteLine("Введен недопустимый цвет"); }
+          
+            Console.ReadKey();
+        }
+    }
+}
