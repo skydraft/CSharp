5c1e979d57b03ff4bfd97f815022ad66d31e6a38
skydraft <wertd@mail.ru>
2018-10-15 15:53:34 +0700

    OOP9:выполнены все задания

--- HomeWorkOOP9/HomeWorkOOP9_2/Program.cs
+++ HomeWorkOOP9/HomeWorkOOP9_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,25 +12,51 @@ namespace HomeWorkOOP9_2
      * и возвращает среднее арифметическое возвращаемых значений методов сообщенных с делегатами в массиве.
      * Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int.
      */
-    delegate int DelArray(int[] arrays);
-    delegate int AllArray(DelArray[] allArrays);
+    delegate int DelRendom();
+   
+    delegate double AllArray(DelRendom[] allArrays);
     class Program
     {
+        private static Random random = new Random();
+         static int MyRendom()=> random.Next(100);
+   
+       
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int[] myArray =  { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            DelArray delArray0;
-            delArray0 = (arrays)=>{ int i = random.Next(0, 8); return arrays[i]; };
-            delArray0.Invoke(myArray);
-            DelArray delArray1;
-            delArray1 = (arrays) => { int i = random.Next(0, 8); return arrays[i]; };
-            delArray1.Invoke(myArray);
-            DelArray delArray3;
-            delArray3 = (arrays) => { int i = random.Next(0, 8); return arrays[i]; };
-            delArray3.Invoke(myArray);
-            DelArray[] delArrays = { delArray0, delArray1, delArray3 };
-            // AllArray allArray = (delArrays) => { return (delArray0 + delArray1 + delArray3) / 3; };
+           // создаем массив с делегатами
+            var arrays = new DelRendom[3];
+            for (int i = 0; i < arrays.Length; i++)
+            {
+
+                //запись arrays[i]= MyRendom; аналогична arrays[i] = ()=>new DelRendom(MyRendom).Invoke();
+                arrays[i]= () => new DelRendom(MyRendom).Invoke();
+
+            }
+            double count = 0;
+            //создаем экземпляр класса-делегата AllArray и сообщаем с ним анонимный метод
+            AllArray allArray = delegate(DelRendom[] delRendoms)
+            {
+
+                foreach (var t in delRendoms)
+                {
+                    count+= t.Invoke();
+                  
+                }
+
+               count =count/ 3;
+                return count/3;
+            };
+            //выводим значения лямбда-выражения сообщенного с делегатом
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                Console.Write(arrays[i].Invoke() + " "); //Вызов лямбда-выражения сообщенного с делегатом
+            }
+
+
+            allArray(arrays);
+            Console.WriteLine("\n"+count);
+          
+
             Console.ReadKey();
         }
     }
