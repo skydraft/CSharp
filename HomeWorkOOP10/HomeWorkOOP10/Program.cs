90cdbcb2a0561ab9de871500576d33da153aa839
skydraft <wertd@mail.ru>
2018-10-16 10:01:16 +0700

    OOP10:выполнено первое задание(фабричный метод)

---
+++ HomeWorkOOP10/HomeWorkOOP10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkOOP10
+{
+    /* Задание 
+     * Создайте класс MyClass<T>, содержащий статический фабричный метод – T FacrotyMethod(),
+     * который будет порождать экземпляры типа,
+     * указанного в качестве параметра типа (указателя места заполнения типом – Т). 
+     */
+    class  MyClass<T> where T : new()
+    {
+
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> myint= new MyClass<int>();
+            Console.WriteLine(myint.GetType());
+            Console.ReadKey();
+           
+        }
+    }
+}
